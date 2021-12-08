77f5a708c5b3a3cf908ea73bc6985781fee4a74d
bkfwebdev <bryant.feld@gmail.com>
2021-12-07 19:08:06 -0500

    an import error was raised

--- website/__init__.py
+++ website/__init__.py
@@ -1,20 +1,35 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "restaurauntbuddy"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'SINAWALI'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'siunimtauchumkiubiuje'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Over9000@localhost:5432/{DB_NAME}"
+
     db.init_app(app)
 
-    from.views import views
-    from.auth import auth
+    from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+    
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+    
+  
+
+
 
