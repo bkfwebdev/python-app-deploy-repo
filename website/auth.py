a01d574e05181aa5db45e511074672c6a68a2316
bkfwebdev <bryant.feld@gmail.com>
2021-12-15 23:17:06 -0500

    login logic added - untested

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,26 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User 
+from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-auth = Blueprint('auth',__name__)
+auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods = ['GET','POST'])
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", testData = "this is a test string")
+   if request.method == 'POST':
+       email = request.form.get('email')
+       password = request.form.get('password')
+       user = User.query.filter_by(email=email).first()
+       if user:
+           if check_password_hash(user.password, password):
+               flash('Logged in succesfully', category='success')
+           else:
+               flash('Login error, please verify login credentials',
+                     category='error')
+       else:
+           flash('Email/User Does not exist.', category='error')
+   return render_template("login.html")
+
 
 @auth.route('/logout')
 def logout():
@@ -21,6 +33,11 @@ def register():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email = email).first()
+        if user:
+            flash('This user already exist.', category = 'error')
+            
 
         if len(email) < 4:
             flash('Email must be greater than four characters.', category = 'error')
