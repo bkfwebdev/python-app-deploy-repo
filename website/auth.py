512030d5d11e1bad8b2c8b8c766b1f7870895683
bkfwebdev <bryant.feld@gmail.com>
2021-12-17 10:46:53 -0500

    added logic to protect routes - needs testing

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
 
@@ -10,10 +11,13 @@ def login():
    if request.method == 'POST':
        email = request.form.get('email')
        password = request.form.get('password')
+
        user = User.query.filter_by(email=email).first()
        if user:
            if check_password_hash(user.password, password):
                flash('Logged in succesfully', category='success')
+               login_user(user, remember = True)
+               return redirect(url_for('views.home'))
            else:
                flash('Login error, please verify login credentials',
                      category='error')
@@ -23,8 +27,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>Logout Page</h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/register', methods = ['GET','POST'])
 def register():
@@ -33,13 +39,10 @@ def register():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
         user = User.query.filter_by(email = email).first()
         if user:
-            flash('This user already exist.', category = 'error')
-            
-
-        if len(email) < 4:
+            flash('This user already exist.', category = 'error')           
+        elif len(email) < 4:
             flash('Email must be greater than four characters.', category = 'error')
         elif len(first_name) < 2:
             flash('First name must be greater than two characters', category = 'error')
@@ -52,6 +55,7 @@ def register():
             new_user = User(email = email, first_name = first_name,password = generate_password_hash(password1, method = 'sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember = True)
             flash('Account created!', category = 'success')
             return redirect(url_for('views.home'))
 
