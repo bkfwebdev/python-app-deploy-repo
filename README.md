26fc034fef5ac61fc4e50efbd205c5675b8f33ad
bkfwebdev <bryant.feld@gmail.com>
2022-04-04 21:49:56 -0400

    index.js update to remove search item added to favorites

---
+++ README.md
@@ -0,0 +1,16 @@
+The reastauraunt buddy/food buddy app is an app to keep a list of restauruants you enjoy eating at 
+If you are a new town or city that you are not familiar with you can do a restauraunt search and add search results to your favorites
+
+Once you register and log in you can add favorites manually using a form on the home page
+Alternatively you can go to the search page to enter a city in the search box to get a list of restauraunts in that city
+
+If you click on a search result it will be removed from the search results and added to your favorites
+favorites can be added manually or deleted from the home page
+
+This project uses Python Flask SQLite and some JavaScript
+
+The worldwide-restauraunts API is used to do restauraunt searches
+
+More info on that here...
+https://rapidapi.com/ptwebsolution/api/worldwide-restaurants/
+
--- website/static/index.js
+++ website/static/index.js
@@ -21,59 +21,20 @@ dataNodes.forEach(dataNode => {
     if (dataNode.className == "name"){formData.append("RestaurauntName",dataNode.textContent)}
     if (dataNode.className == "phone"){formData.append("PhoneNumber",dataNode.textContent)}
     if (dataNode.className == "website"){formData.append("Website",dataNode.textContent)}
-    if (dataNode.className == "address"){formData.append("StreetAddress",dataNode .textContent)}
+    if (dataNode.className == "address"){formData.append("StreetAddress",dataNode.textContent)}
     }   
 });
 
-
-/*
-Array.prototype.forEach.call (dataNodes, function (node) {
-    if (node.nextElementSibling.className == "name"){formData.append("RestaurauntName",node.textContent)}
-    if (node.nextElementSibling.className == "phone"){formData.append("PhoneNumber",node.textContent)}
-    if (node.nextElementSibling.className == "website"){formData.append("Website",node.textContent)}
-    if (node.nextElementSibling.className == "address"){formData.append("StreetAddress",node.textContent)}
-
-        if(!element.nextElementSibling){return}
-    if (element.nextElementSibling.className == "name"){formData.append("RestaurauntName",element.textContent)}
-    if (element.nextElementSibling.className == "phone"){formData.append("PhoneNumber",element.textContent)}
-    if (element.nextElementSibling.className == "website"){formData.append("Website",element.textContent)}
-    if (element.nextElementSibling.className == "address"){formData.append("StreetAddress",element.textContent)}
-
-} );
-*/
 console.log(formData)
 fetch("/",
     {
         body: formData,
         method: "POST"
     });
-
+li.remove();
 console.log("function complete")
 }
 
 function eventCaptureTest(){
     alert("event captured")
 }
-
-/*submit data for new favorite to data base
-
-
-formData.append("username", "Groucho");
-formData.append("accountnum", 123456); // number 123456 is immediately converted to a string "123456"
-
-// HTML file input, chosen by user
-formData.append("userfile", fileInputElement.files[0]);
-
-// JavaScript file-like object
-var content = '<a id="a"><b id="b">hey!</b></a>'; // the body of the new file...
-var blob = new Blob([content], { type: "text/xml"});
-
-formData.append("webmasterfile", blob);
-
-var request = new XMLHttpRequest();
-request.open("POST", "http://foo.com/submitform.php");
-request.send(formData);
-}
-
-
-*/
