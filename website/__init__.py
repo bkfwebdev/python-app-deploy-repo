4919929aaafa1efd4b431bb7cfafc983052c63aa
bkfwebdev <bryant.feld@gmail.com>
2022-04-26 21:14:55 -0400

    works locally with heroku db

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'siunimtauchumkiubiuje'
     #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     #app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:Over9000@localhost:5432/{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://ruzouuemtotljl:0ff662697a7a1f4cbf22dff699770508865457921c0690195ea8526409013284@ec2-54-157-79-121.compute-1.amazonaws.com:5432/d36aemg0ue2hkv/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ruzouuemtotljl:0ff662697a7a1f4cbf22dff699770508865457921c0690195ea8526409013284@ec2-54-157-79-121.compute-1.amazonaws.com:5432/d36aemg0ue2hkv'
 
 
 
